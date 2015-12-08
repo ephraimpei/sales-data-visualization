cc742f6b077bf0b76063be5f623291e7694ea6f4
Ephraim Pei <ephdawg3374@gmail.com>
2015-12-08 16:17:05 -0500

    .gitignore added. test data file added

---
+++ javascript/data.js
@@ -0,0 +1,55 @@
+var families = ["Family A", "Family B"];
+var brands = ["Brand W", "Brand X", "Brand Y", "Brand Z"];
+
+var products =[
+  {
+    name: "Product 1",
+    brand: "Brand W",
+    family: "Family A"
+  },
+  {
+    name: "Product 2",
+    brand: "Brand W",
+    family: "Family A"
+  },
+  {
+    name: "Product 3",
+    brand: "Brand X",
+    family: "Family A"
+  },
+  {
+    name: "Product 4",
+    brand: "Brand X",
+    family: "Family A"
+  },
+  {
+    name: "Product 5",
+    brand: "Brand X",
+    family: "Family A"
+  },
+  {
+    name: "Product 6",
+    brand: "Brand Y",
+    family: "Family B"
+  },
+  {
+    name: "Product 7",
+    brand: "Brand Y",
+    family: "Family B"
+  },
+  {
+    name: "Product 8",
+    brand: "Brand Y",
+    family: "Family B"
+  },
+  {
+    name: "Product 9",
+    brand: "Brand Z",
+    family: "Family B"
+  },
+  {
+    name: "Product 10",
+    brand: "Brand Z",
+    family: "Family B"
+  }
+];
