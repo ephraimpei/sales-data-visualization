913d5a14c3327a9953de37aac25f37e8ad9cf933
Ephraim Pei <ephdawg3374@gmail.com>
2015-12-09 19:26:32 -0500

    basic tree structure created with basic settings

--- javascript/main.js
+++ javascript/main.js
@@ -5,41 +5,65 @@ function main() {
         i = 0,
         root = {};
 
-  var tree = d3.layout.tree();
-  var circles = {};
-  var paths = {};
-  var labels = {};
+  // tree.children(function (d) { return d.values; }).size([h, w]);
 
-  tree.children(function (d) { return d.values; }).size([h, w]);
+  var canvas = d3.select(".main-graph").append("svg")
+    .attr("width", w + m[1] + m[3])
+    .attr("height", h + m[0] + m[2])
+    .append("g")
+      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
   var diagonal = d3.svg.diagonal()
        .projection(function (d) {
            return [d.y, d.x];
        });
 
-  var svg = d3.select(".main-graph").append("svg:svg")
-    .attr("width", w + m[1] + m[3])
-    .attr("height", h + m[0] + m[2])
-    .append("svg:g")
-    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+  canvas.append("path")
+    .attr("fill", "none")
+    .stroke("stroke", "black")
+    .attr("d", diagonal);
 
-  var nodeRadius;
+  var tree = d3.layout.tree()
+    .size([600, 600]);
 
   // read test data
   d3.csv("data/sales_data.csv", function (csv) {
-    var data = [];
+    var rawData = [];
+
+    csv.forEach(function (salesRecord) {
+      rawData.push(salesRecord);
+    });
     debugger;
-    var nest = d3.nest()
-      .key(function (d) {
-        return d.Level1;
-      })
-      .key(function (d) {
-        return d.Level2;
-      })
-      .key(function (d) {
-        return d.Level3;
-      });
   });
+
+  var nodes = tree.nodes(data);
+
+  var links = tree.links(nodes);
+
+  var node = canvas.selectAll(".node")
+    .data(nodes)
+    .enter()
+    .append("g")
+      .attr("class", "node")
+      .attr("transform", "translate(" + d.y + "," + d.x + ")");
+
+  node.append("circle")
+    .attr("r", 5)
+    .attr("fill", "steelblue");
+
+  node.append("text")
+    .text(function (d) {
+      return d.name;
+    });
+
+  canvas.selectAll(".link")
+    .data(links)
+    .enter()
+    .append("path")
+    .attr("class", "link")
+    .attr("fill", "none")
+    .attr("stroke", "#ADADAD")
+    .attr("d", diagonal);
 }
 
 main();
